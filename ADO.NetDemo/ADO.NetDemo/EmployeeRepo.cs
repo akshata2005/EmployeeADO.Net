2e3c1a700d8ff568e54d644d41ad70c26f2d4fec
akshata2005 <akshatasawant1805@gmail.com>
2022-02-19 22:08:39 +0530

    [akshata] Add: Database connection

---
+++ ADO.NetDemo/ADO.NetDemo/EmployeeRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NetDemo
+{
+    public class EmployeeRepo
+    {
+        public static string Connectionstring = "Data Source=DESKTOP-711IFBF/SQLEXPRESS;Initial Catalog=payroll_service;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static void GetAllEmployee()
+        {
+           SqlConnection sqlConnection = new SqlConnection(Connectionstring);
+        }
+    }
+}
